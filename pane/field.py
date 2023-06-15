95c2225de05abd4e69fc1da43e59eed075455e97
Colin Gilgenbach <colin@gilgenbach.net>
2023-06-15 13:04:08 -0400

    Started on field renaming

--- pane/field.py
+++ pane/field.py
@@ -1,28 +1,71 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+import itertools
+import re
 import typing as t
-from typing import Generic, Optional, TypeVar, Type, Callable, Any, Union, cast
-from typing import Sequence, List
-
-
-T = TypeVar('T')
 
 
 class _Missing:
     pass
 
 
+T = t.TypeVar('T')
 _MISSING = _Missing()
+
+
+RenameStyle = t.Literal['snake', 'camel', 'pascal', 'kebab', 'scream']
+
+
+CONVERT_FNS: t.Dict[RenameStyle, t.Callable[[t.Sequence[str]], str]] = {
+    'snake': lambda parts: '_'.join(part.lower() for part in parts),
+    'scream': lambda parts: '_'.join(part.upper() for part in parts),
+    'kebab': lambda parts: '-'.join(part.lower() for part in parts),
+    'camel': lambda parts: ''.join(part.lower() if i == 0 else part.title() for (i, part) in enumerate(parts)),
+    'pascal': lambda parts: ''.join(part.title() for part in parts),
+}
+
+
+def pairwise(it: t.Iterable[T]) -> t.Iterator[t.Tuple[T, T]]:
+    it = iter(it)
+    while True:
+        try:
+            a = next(it)
+            b = next(it)
+        except StopIteration:
+            break
+        yield (a, b)
+
+
+def split_field_name(field: str) -> t.Sequence[str]:
+    parts = re.split(r'[_-]', field)
+    if not all(parts):
+        raise ValueError(f"Unable to interpret field '{field}' for automatic rename")
+
+    def split_case(field: str):
+        if field.isupper() or field.islower() or field.istitle():
+            yield field
+            return
+        seps = re.split(r'([A-Z])', field)
+        if seps[0] != '':
+            yield seps[0]
+        for (s1, s2) in pairwise(seps[1:]):
+            yield s1 + s2
+
+    return tuple(itertools.chain.from_iterable(map(split_case, parts)))
+
+
+def rename_field(field: str, style: RenameStyle) -> str:
+    return CONVERT_FNS[style](split_field_name(field))
 
 
 @dataclass(kw_only=True)
 class FieldSpec:
-    aliases: Optional[List[str]] = None
+    aliases: t.Optional[t.Sequence[str]] = None
     save_name: t.Optional[str] = None
     init: bool = True
-    default: Union[t.Any, _Missing] = _MISSING
-    default_factory: Optional[Callable[[], t.Any]] = None
+    default: t.Union[t.Any, _Missing] = _MISSING
+    default_factory: t.Optional[t.Callable[[], t.Any]] = None
     kw_only: bool = False
     flatten: bool = False
 
@@ -33,7 +76,7 @@ class FieldSpec:
         if self.flatten:
             raise NotImplementedError()
 
-    def make_field(self, name: str, ty: Union[type, _Missing] = _MISSING) -> Field:
+    def make_field(self, name: str, ty: t.Union[type, _Missing] = _MISSING) -> Field:
         py_name = name = name
         ty = t.cast(type, t.Any if ty is _MISSING else ty)
         return Field(name=name, py_name=py_name, type=ty, save_name=self.save_name, aliases=self.aliases,
@@ -52,11 +95,11 @@ class Field(FieldSpec):
 
 
 def field(*,
-    aliases: Optional[List[str]] = None,
+    aliases: t.Optional[t.Sequence[str]] = None,
     save_name: t.Optional[str] = None,
     init: bool = True,
-    default: Union[T, _Missing] = _MISSING,
-    default_factory: Optional[Callable[[], T]] = None,
+    default: t.Union[T, _Missing] = _MISSING,
+    default_factory: t.Optional[t.Callable[[], T]] = None,
     kw_only: bool = False,
     flatten: bool = False,
 ) -> t.Any:
---
+++ pane/test_field.py
@@ -0,0 +1,33 @@
+
+import pytest
+
+from .field import split_field_name, rename_field
+
+
+@pytest.mark.parametrize(('field', 'out'), [
+    ('test', ('test',)),
+    ('test_snake_Case', ('test', 'snake', 'Case'),),
+    ('test-KEBAB-Case', ('test', 'KEBAB', 'Case'),),
+    ('TestPascalCase', ('Test', 'Pascal', 'Case'),),
+    ('testCamelCase', ('test', 'Camel', 'Case'),),
+    ('TEST_SCREAM_CASE', ('TEST', 'SCREAM', 'CASE'),),
+    # conservative for now
+    ('__test__', ValueError("Unable to interpret field '__test__' for automatic rename")),
+])
+def test_split_field(field, out):
+    if isinstance(out, ValueError):
+        with pytest.raises(ValueError, match=out.args[0]):
+            split_field_name(field)
+    else:
+        assert split_field_name(field) == out
+
+
+@pytest.mark.parametrize(('field', 'style', 'out'), [
+    ('toPascalCase', 'pascal', 'ToPascalCase'),
+    ('To_CAMEL_case', 'camel', 'toCamelCase'),
+    ('ToScream_Case', 'scream', 'TO_SCREAM_CASE'),
+    ('To_kebab_Case', 'kebab', 'to-kebab-case'),
+    ('ToSnakeCase', 'snake', 'to_snake_case'),
+])
+def test_rename_field(field, style, out):
+    assert rename_field(field, style) == out
